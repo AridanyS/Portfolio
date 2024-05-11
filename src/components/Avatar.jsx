b1eee942428304d7856dcb7abe2178f29a662ed2
Aridany <aridany54@gmail.com>
2024-05-12 01:22:43 +0200

    Avatar adjustement  - Modal Install

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -4,30 +4,11 @@ import { Avatar } from "@nextui-org/avatar";
 
 export default function AvatarPicture() {
     return (
-        <div className="flex items-center gap-4">
-            <Avatar isBordered radius="full" src="./images/foto.jfif" />
-            <Avatar
-                isBordered
-                src="https://i.pravatar.cc/150?u=a04258a2462d826712d"
-                
-            />
-            <Avatar
-                isBordered
-                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-            />
-            <Avatar
-                isBordered
-                src="https://i.pravatar.cc/150?u=a04258114e29026302d"
-            />
-            <Avatar
-                isBordered
-                src="https://i.pravatar.cc/150?u=a04258114e29026702d"
-            />
-            <Avatar
-                isBordered
-                src="https://i.pravatar.cc/150?u=a04258114e29026708c"
-            />
-              <Avatar name="Junior" />
+        <div className="flex items-center justify-center w-screen ">
+            <Avatar src="./images/foto.jfif" className="w-24 h-24"/>
+            <div className="border border-slate-500 border-1 ">
+                Sobre mí
+            </div>
         </div>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
-  content: ["./index.html", "./src/components/Avatar.jsx", "./src/**/*.{html,js}"],
+  content: [
+    "./index.html",
+    "./src/components/Avatar.jsx",
+    "./src/**/*.{html,js}",
+        './node_modules/@nextui-org/theme/dist/components/(avatar|input).js'
+  ],
+
   theme: {
     extend: {
-      fontFamily:{
-        'Cormorant': ["Cormorant Garamond", 'serif'],
-        'Roboto': ["Roboto", 'sans-serif'],
-        'Montserrat': ['Montserrat', 'sans-serif'],
-        'Caslon': ['Libre Caslon', 'Text', 'serif'],
-        'Titillium': ['Titillium Web', 'sans-serif'],
-        'work-sans':['Work Sans', 'sans-serif'],
+      fontFamily: {
+        Cormorant: ["Cormorant Garamond", "serif"],
+        Roboto: ["Roboto", "sans-serif"],
+        Montserrat: ["Montserrat", "sans-serif"],
+        Caslon: ["Libre Caslon", "Text", "serif"],
+        Titillium: ["Titillium Web", "sans-serif"],
+        "work-sans": ["Work Sans", "sans-serif"],
       },
       colors: {
-        "page": "#101010",
+        page: "#101010",
         "page-white": "#FFFFFF",
       },
     },
