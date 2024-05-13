e61626a810b289347ca2e0d113d881a7ad658003
Aridany <aridany54@gmail.com>
2024-05-13 19:26:37 +0200

    Project grid

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,27 +4,29 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/effect-creative";
-import 'swiper/css/effect-cube';
-import 'swiper/css/pagination';
-import { EffectCube, Pagination } from 'swiper/modules';
+import "swiper/css/effect-cube";
+import "swiper/css/pagination";
+import { EffectCube, Pagination } from "swiper/modules";
 
 //import { EffectCreative } from "swiper/modules";
 
 export default function ProjectsGrid() {
   return (
     <>
-
-      <article className="container w-[70%] mx-auto ">
+      <article
+        className="container w-[50%] mx-auto absolute top-[20%] 
+      left-[25%]"
+      >
         <Swiper
-          effect={'cube'}
+          effect={"cube"}
           grabCursor={true}
           cubeEffect={{
-            shadow: false,
+            shadow: true,
             slideShadows: true,
             shadowOffset: 20,
             shadowScale: 0.94,
           }}
-          pagination={true}
+          pagination={false}
           modules={[EffectCube, Pagination]}
           className="mySwiper"
         >
@@ -32,25 +34,41 @@ export default function ProjectsGrid() {
             <img
               src="../images/Drag-Drop.png"
               alt="web_drag-drop"
-              className=""
+              className="h-[350px]"
             ></img>
+            <div className="bg-gray-500">
+              <span>Drag drop</span>{" "}
+              <a
+                className="cursor-pointer"
+                href="https://drag-drop-khaki.vercel.app"
+              >
+                Deploy
+              </a>
+            </div>
           </SwiperSlide>
           <SwiperSlide className="">
             <img
               src="../images/chiwawa.png"
               alt="restaurante_chiwawa"
-              className=""
+              className="h-[350px]"
             ></img>
+            chiwawa
           </SwiperSlide>
           <SwiperSlide className="">
             <img
               src="../images/rocket.png"
               alt="rocket"
-              className=""
+              className="h-[350px]"
             ></img>
+            Rocket
           </SwiperSlide>
           <SwiperSlide className="">
-            <img src="../images/airbnb.png" alt="airbnb" className=""></img>
+            <img
+              src="../images/airbnb.png"
+              alt="airbnb"
+              className="h-[350px]"
+            ></img>
+            <div>Airbnb</div>
           </SwiperSlide>
         </Swiper>
       </article>
