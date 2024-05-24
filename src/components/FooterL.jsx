63a11d67507400125f82179f818ef2f4858a8fc3
Aridany <aridany54@gmail.com>
2024-05-25 00:51:35 +0200

    Bugs SEO, resize Icons footer

--- src/components/FooterL.jsx
+++ src/components/FooterL.jsx
@@ -9,7 +9,7 @@
 
   export default function FooterPortfolio() {
     return (
-      <div className="flex h-[8vh] justify-evenly flex-wrap md:gap-3 items-center p-3 max-sm:text-[10px] lg:text-lg text-text-modal-light bg-background-DarkBrown">
+      <div className="flex h-[10vh] justify-evenly flex-wrap gap-1 md:gap-3 items-center p-3 max-sm:text-[13px] lg:text-lg text-text-modal-light bg-background-DarkBrown">
         <div><a href="https://github.com/AridanyS/Portfolio" className="hover:underline" target="_blank">This page</a> is developed by <a href="https://github.com/AridanyS" className="hover:underline" target="_blank">Aridany</a></div>
 
         <div className="flex items-center md:gap-3 ">
@@ -18,20 +18,20 @@
           
           <span className="flex items-center gap-1 ">
             <a href="https://lucide.dev/" target="_blank" 
-            className="w-[15px] lg:w-[25px] hover:opacity-50">
+            className="w-[20px] lg:w-[25px] hover:opacity-50">
               <img src="/logo.dark.svg"
               alt="Lucide"></img>
             </a>
 
             <a href="https://svgl.app/?search=svg" target="_blank"
-            className="w-[15px] lg:w-[25px] hover:opacity-50">
+            className="w-[20px] lg:w-[25px] hover:opacity-50">
               <img src="/svgl.svg"
               alt="svgl_pheralb"></img>
             </a>
         
           </span>
           <span className="mx-1">|</span>
-          <a href="https://mit-license.org/" className=" hover:underline" target="_blank">MIT license </a><img src="/scale.svg" className="w-[13px] lg:w-[25px] bg-text-modal-light opacity-80 rounded m-1 " alt="MIT"></img>
+          <a href="https://mit-license.org/" className=" hover:underline" target="_blank">MIT license </a><img src="/scale.svg" className="w-[18px] lg:w-[25px] bg-text-modal-light opacity-80 rounded m-1 " alt="MIT"></img>
         </div>
 
         
