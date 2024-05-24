04b06d957969f7eb30724513ca5025597759b362
Aridany <aridany54@gmail.com>
2024-05-24 12:46:13 +0200

    footer styles

--- src/components/FooterL.jsx
+++ src/components/FooterL.jsx
@@ -2,14 +2,29 @@
         Developed by aridany
         Siéntete libre de usar todo lo que veas (Abrazo)
         made with heart by ...
-        Location?¿
+        !!!Location?¿!!!
         poner tmb contact aqui
+        <a href="https://github.com/AridanyS/Portfolio"></a>
 */
 
 export default function FooterPortfolio() {
   return (
-    <div className="border-2 text-slate-50 bg-background-DarkBrown">
-        Ñañañañ34
+    <div className="flex flex-col flex-wrap items-center justify-center gap-2 p-5 text-xs text-text-modal-light bg-background-DarkBrown">
+      <div>Feel free to use every components on <a href="https://github.com/AridanyS/Portfolio" className="hover:underline" target="_blank">this page</a> 🤗</div>
+      <div className="flex items-center justify-center gap-3">
+        Icons from:
+        <span className="flex items-center gap-3">
+          <a href="https://lucide.dev/" target="_blank" 
+          className="w-[15px] hover:opacity-50">
+            <img src="/logo.dark.svg"></img>
+          </a>
+
+          <a href="https://svgl.app/?search=svg" target="_blank"
+          className="w-[15px] hover:opacity-50">
+            <img src="/svgl.svg"></img>
+          </a>
+        </span>
+      </div>
     </div>
-  )
+  );
 }
