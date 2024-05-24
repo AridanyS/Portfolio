01ab71b740ca8a1396dce3ec8c680307d2beac30
Aridany <aridany54@gmail.com>
2024-05-24 15:15:07 +0200

    styles on footer

--- src/components/FooterL.jsx
+++ src/components/FooterL.jsx
@@ -12,9 +12,10 @@
       <div className="flex h-[5vh] justify-evenly flex-wrap md:gap-3 items-center p-1 max-sm:text-[9px] text-xs text-text-modal-light bg-background-DarkBrown">
         <div><a href="https://github.com/AridanyS/Portfolio" className="hover:underline" target="_blank">This page</a> is developed by <a href="https://github.com/AridanyS" className="hover:underline" target="_blank">Aridany</a></div>
 
-        <div className="flex items-center gap-1 md:gap-3 ">
-         
+        <div className="flex items-center md:gap-3 ">
+        
           Icons:
+          
           <span className="flex items-center gap-1 ">
             <a href="https://lucide.dev/" target="_blank" 
             className="w-[10px] hover:opacity-50">
@@ -25,9 +26,10 @@
             className="w-[10px] hover:opacity-50">
               <img src="/svgl.svg"></img>
             </a>
+        
           </span>
-          <span className="opacity-0 md:opacity-100">|</span>
-          <a href="https://mit-license.org/" className=" hover:underline" target="_blank">MIT license </a><img src="/scale.svg" className="w-[10px] bg-text-modal-light opacity-80 rounded "></img>
+          <span className="mx-1">|</span>
+          <a href="https://mit-license.org/" className=" hover:underline" target="_blank">MIT license </a><img src="/scale.svg" className="w-[10px] bg-text-modal-light opacity-80 rounded m-1 "></img>
         </div>
 
         
